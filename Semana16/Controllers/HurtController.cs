1a5ed2c9d590d998ceb46a9f552565ea195c11c7
Edvin Guevara <Edvin Guevara@DESKTOP-JEKA2DM>
2022-11-18 08:02:09 -0600

    Agregar archivos de proyecto.

---
+++ Semana16/Controllers/HurtController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana16.Controllers
+{
+    public class HurtController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Welcome(string bienvenida, string saludo, int edad)
+        {
+            ViewData["bienvenida"] = "Buenos dias " + bienvenida;
+            ViewData["saludo"] = "Espero encuentres lo que buscas " + saludo;
+            ViewData["edad"] = edad;
+
+
+            return View();
+        }
+    }
+}
---
+++ Semana16/Data/semana16Context.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Semana16.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Semana16.Data
+{
+    public class semana16Context : DbContext
+    {
+
+        public semana16Context(DbContextOptions<semana16Context> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
